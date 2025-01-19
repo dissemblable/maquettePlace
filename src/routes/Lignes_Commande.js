43ef2b85578534df308c20aa63703392cdf4b329
dissemblable <julesvallet23@gmail.com>
2025-01-19 21:40:52 +0100

    securing endpoint ligne commande

--- src/routes/Lignes_Commande.js
+++ src/routes/Lignes_Commande.js
@@ -17,9 +17,10 @@ export const Lignes_CommandesRouter = (db) =>
   })
   
   .get("/:id", async (req, res) => {
-    const { id } = req.params;
     try {
-      const [results] = await db.query(`SELECT * FROM Lignes_Commandes WHERE id = ${id}`); // Injection SQL possible ici
+      const [results] = await db.query(`SELECT * FROM Lignes_Commandes WHERE id = ?`,
+        [req.params.id]
+      );
       res.json(results);
     } catch (err) {
       res.status(500).json({ error: "Erreur lors de la récupération du Lignes_Commandes." });
@@ -29,27 +30,31 @@ export const Lignes_CommandesRouter = (db) =>
   .post("/", async (req, res) => {
     const { nom, commande_id, produit_id, quantite, prix_unitaire } = req.body;
     try {
-      const [results] = await db.query(`INSERT INTO Lignes_Commandes (nom, commande_id, produit_id, quantite, prix_unitaire) VALUES ('${nom}', ${commande_id}, ${produit_id}, ${quantite}, ${prix_unitaire})`); // Injection SQL possible ici
-      res.status(201).json({ id: results.insertId, ...req.body });
+      const [results] = await db.query(`INSERT INTO Lignes_Commandes (nom, commande_id, produit_id, quantite, prix_unitaire) VALUES (?, ?, ?, ?, ?)`,
+        [nom, commande_id, produit_id, quantite, prix_unitaire]
+      );
+      res.status(201).json({ id: results.insertId, nom, commande_id, produit_id, quantite, prix_unitaire });
     } catch (err) {
       res.status(500).json({ error: "Erreur lors de la création du Lignes_Commandes." });
     }
   })
   
   .put("/:id", async (req, res) => {
-    const { id } = req.params;
     const { nom, commande_id, produit_id, quantite, prix_unitaire } = req.body;
     try {
-      const [results] = await db.query(`UPDATE Lignes_Commandes SET nom = '${nom}', commande_id = ${commande_id}, produit_id = ${produit_id}, quantite = ${quantite}, prix_unitaire = ${prix_unitaire} WHERE id = ${id}`); // Injection SQL possible ici
+      const [results] = await db.query(`UPDATE Lignes_Commandes SET nom = ?, commande_id = ?, produit_id = ?, quantite = ?, prix_unitaire = ? WHERE id = ?`,
+        [nom, commande_id, produit_id, quantite, prix_unitaire, req.params.id]
+      );
     } catch (err) {
       res.status(500).json({ error: "Erreur lors de la mise à jour du Lignes_Commandes." });
     }
   })
   
   .delete("/:id", async (req, res) => {
-    const { id } = req.params;
     try {
-      const [results] = await db.query(`DELETE FROM Lignes_Commandes WHERE id = ${id}`); // Injection SQL possible ici
+      const [results] = await db.query(`DELETE FROM Lignes_Commandes WHERE id = ?`,
+        [req.params.id]
+      );
     } catch (err) {
       res.status(500).json({ error: "Erreur lors de la suppression du Lignes_Commandes." });
     }
