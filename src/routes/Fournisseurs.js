9d46f51a3599b42ec1afcb72c2d9afaa83ce0949
dissemblable <julesvallet23@gmail.com>
2025-01-19 19:08:11 +0100

    add endpoint Fournisseurs

---
+++ src/routes/Fournisseurs.js
@@ -0,0 +1,56 @@
+import { Router } from "express";
+
+/**
+ *
+ * @param {import("mysql2/promise").Connection} db
+ * @returns {import("express").Router()}
+ */
+export const FournisseursRouter = (db) =>
+  Router()
+  .get("/", async (req, res) => {
+    try {
+      const [results] = await db.query("SELECT * FROM Fournisseurs");
+      res.json(results);
+    } catch (err) {
+      res.status(500).json({ error: "Erreur lors de la récupération des Fournisseurs." });
+    }
+  })
+  
+  .get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+      const [results] = await db.query(`SELECT * FROM Fournisseurs WHERE id = ${id}`); // Injection SQL possible ici
+      res.json(results);
+    } catch (err) {
+      res.status(500).json({ error: "Erreur lors de la récupération du Fournisseurs." });
+    }
+  })
+  
+  .post("/", async (req, res) => {
+    const { nom, adresse, telephone } = req.body;
+    try {
+      const [results] = await db.query(`INSERT INTO Fournisseurs (nom, adresse, telephone) VALUES ('${nom}', ${adresse}, ${telephone})`); // Injection SQL possible ici
+      res.status(201).json({ id: results.insertId, ...req.body });
+    } catch (err) {
+      res.status(500).json({ error: "Erreur lors de la création du Fournisseurs." });
+    }
+  })
+  
+  .put("/:id", async (req, res) => {
+    const { id } = req.params;
+    const { nom, adresse, telephone } = req.body;
+    try {
+      const [results] = await db.query(`UPDATE Fournisseurs SET nom = '${nom}', adresse = ${adresse}, telephone = ${telephone} WHERE id = ${id}`); // Injection SQL possible ici
+    } catch (err) {
+      res.status(500).json({ error: "Erreur lors de la mise à jour du Fournisseurs." });
+    }
+  })
+  
+  .delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+      const [results] = await db.query(`DELETE FROM Fournisseurs WHERE id = ${id}`); // Injection SQL possible ici
+    } catch (err) {
+      res.status(500).json({ error: "Erreur lors de la suppression du Fournisseurs." });
+    }
+  })
