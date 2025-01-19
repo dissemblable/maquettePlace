2793532608259b9b9d127e6a8a2a788f3a574f26
dissemblable <julesvallet23@gmail.com>
2025-01-19 21:42:50 +0100

    securing endpoint produit fournisseur

--- src/routes/Produits_Fournisseurs.js
+++ src/routes/Produits_Fournisseurs.js
@@ -17,9 +17,10 @@ export const Produits_FournisseursRouter = (db) =>
   })
   
   .get("/:id", async (req, res) => {
-    const { id } = req.params;
     try {
-      const [results] = await db.query(`SELECT * FROM Produits_Fournisseurs WHERE id = ${id}`); // Injection SQL possible ici
+      const [results] = await db.query(`SELECT * FROM Produits_Fournisseurs WHERE id = ?`,
+        [req.params.id]
+      );
       res.json(results);
     } catch (err) {
       res.status(500).json({ error: "Erreur lors de la récupération du produit_Fournisseurs." });
@@ -29,27 +30,31 @@ export const Produits_FournisseursRouter = (db) =>
   .post("/", async (req, res) => {
     const { nom, produit_id, fournisseur_id } = req.body;
     try {
-      const [results] = await db.query(`INSERT INTO Produits_Fournisseurs (nom, produit_id fournisseur_id) VALUES ('${nom}', ${produit_id}, ${fournisseur_id})`); // Injection SQL possible ici
-      res.status(201).json({ id: results.insertId, ...req.body });
+      const [results] = await db.query(`INSERT INTO Produits_Fournisseurs (nom, produit_id fournisseur_id) VALUES (?, ?, ?)`,
+        [nom, produit_id, fournisseur_id]
+      );
+      res.status(201).json({ id: results.insertId, nom, produit_id, fournisseur_id });
     } catch (err) {
       res.status(500).json({ error: "Erreur lors de la création du produit_Fournisseurs." });
     }
   })
   
   .put("/:id", async (req, res) => {
-    const { id } = req.params;
     const { nom, produit_id, fournisseur_id } = req.body;
     try {
-      const [results] = await db.query(`UPDATE Produits_Fournisseurs SET nom = '${nom}', produit_id = ${produit_id}, fournisseur_id = ${fournisseur_id} WHERE id = ${id}`); // Injection SQL possible ici
+      const [results] = await db.query(`UPDATE Produits_Fournisseurs SET nom = ?, produit_id = ?, fournisseur_id = ? WHERE id = ?`,
+        [nom, produit_id, fournisseur_id, req.params.id]
+      );
     } catch (err) {
       res.status(500).json({ error: "Erreur lors de la mise à jour du Produits_Fournisseurs." });
     }
   })
   
   .delete("/:id", async (req, res) => {
-    const { id } = req.params;
     try {
-      const [results] = await db.query(`DELETE FROM Produits_Fournisseurs WHERE id = ${id}`); // Injection SQL possible ici
+      const [results] = await db.query(`DELETE FROM Produits_Fournisseurs WHERE id = ?`,
+        [req.params.id]
+      );
     } catch (err) {
       res.status(500).json({ error: "Erreur lors de la suppression du Produits_Fournisseurs." });
     }
